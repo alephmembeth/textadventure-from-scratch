1390ebe48ed8af0f0f631f685f942d57e5214b74
Max <53583040+alephmembeth@users.noreply.github.com>
2023-03-25 10:34:17 +0100

    Implement history limit

--- game.gd
+++ game.gd
@@ -1,11 +1,10 @@
 extends Control
 
-
 const InputResponse = preload("res://input_response.tscn")
 
+@export var max_lines_remembered := 42
 
 var max_scroll_length := 0
-
 
 @onready var history = $background/margins/rows/info_panel/scroll/history
 @onready var scroll = $background/margins/rows/info_panel/scroll
@@ -24,6 +23,14 @@ func handle_scrollbar_changed():
 
  
 func _on_input_line_text_submitted(new_text: String) -> void:
+	if new_text.is_empty() or new_text.begins_with(" "): 
+		return
+
 	var input_response = InputResponse.instantiate()
 	input_response.set_text(new_text, "Okay, cool.")
 	history.add_child(input_response)
+
+	if history.get_child_count() > max_lines_remembered:
+		var rows_to_forget = history.get_child_count() - max_lines_remembered
+		for i in range(rows_to_forget):
+			history.get_child(i).queue_free()
